d5033aaa664de17763b2a97fa558508f13418301
kishore951 <kishoresakthivel951@gmail.com>
2022-10-11 17:58:14 +0530

    tue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ViewAllAirlinesComponent } from './view-all-airlines/view-all-airlines.component';
+import { ViewAllPassengersComponent } from './view-all-passengers/view-all-passengers.component';
 
 const appRoutes:Routes=[
   {
     path:"",component:ViewAllAirlinesComponent
+  },
+  {
+    path:"search",component:ViewAllPassengersComponent
   }
 ]
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ViewAllAirlinesComponent
+    ViewAllAirlinesComponent,
+    ViewAllPassengersComponent,
   ],
   imports: [
     BrowserModule,
